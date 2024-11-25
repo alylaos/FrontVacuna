534e8c5f6e542309e450a8b0f2ebed1dcd1673fd
unknown <aly_laos@outlook.com>
2024-11-25 01:47:31 -0500

    prueba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,20 @@ import { CreateProductComponent } from './create-product/create-product.componen
 import { HomeProductComponent } from './home-product/home-product.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductListMaterialComponent } from './product-list-material/product-list-material.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { SedesComponent } from './sedes/sedes.component';
+import { RecordatorioComponent } from './recordatorio/recordatorio.component';
+import { VacunasComponent } from './vacunas/vacunas.component';
 
 export const routes: Routes = [
     {path: "list", component: ProductListComponent},
     {path: "material", component: ProductListMaterialComponent},
     {path: "create", component: CreateProductComponent},
     {path: "home", component: HomeProductComponent},
+    {path: "cliente", component: ClienteComponent},
+    { path: "sedes", component: SedesComponent },
+    { path: "recordatorio", component: RecordatorioComponent },
+    { path: 'vacunas', component: VacunasComponent },
     {path: '', redirectTo: '/home', pathMatch: "full"},
     {path: '**', component: PageNotFoundComponent}
 ];
---
+++ src/app/vacunas/vacunas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vacunas',
+  standalone: true,
+  imports: [],
+  templateUrl: './vacunas.component.html',
+  styleUrl: './vacunas.component.css'
+})
+export class VacunasComponent {
+
+}
